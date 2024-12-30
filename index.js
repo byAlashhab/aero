cceae7e4c20530c26da0e96ab3000b14dd908035
Abdullah Alashhab <abdullahalshhab@gmail.com>
2024-12-30 02:01:19 +0200

    feat: fp

--- index.js
+++ index.js
@@ -1,46 +1,66 @@
 const fs = require("fs");
 
-const path = __filename.slice(0, __filename.length - 13);
+const path = __filename.slice(0, __filename.length - 26);
 
 class Aero {
   async New(name, headers, data) {
-    if (fs.existsSync(`${path}/aero/${name}.csv`)) {
-      throw new Error("Table already exists");
-    }
-
-    if (!fs.existsSync(`${path}/aero`)) {
-      try {
-        fs.mkdirSync(`${path}/aero`);
-      } catch (error) {
-        console.log(error);
+    try {
+      if (fs.existsSync(`${path}/db/${name}.csv`)) {
+        throw new Error("Table already exists");
       }
+
+      if (!fs.existsSync(`${path}/db`)) {
+        try {
+          fs.mkdirSync(`${path}/db`);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      const csvData = [
+        headers.join(","),
+        ...data.map((row) => row.join(",")),
+      ].join("\n");
+
+      fs.writeFileSync(`${path}/db/${name}.csv`, csvData);
+    } catch (error) {
+      console.log(error);
     }
-
-    const csvData = [
-      headers.join(","),
-      ...data.map((row) => row.join(",")),
-    ].join("\n");
-
-    fs.writeFileSync(`${path}/aero/${name}.csv`, csvData);
   }
 
-  async Add() {}
+  async Add(name, data) {
+    try {
+      if (!fs.existsSync(`${path}/db/${name}.csv`)) {
+        throw new Error("Table doesn't exists");
+      }
+
+      data = data.map((row) => row.join(",")).join("\n");
+
+      fs.appendFileSync(`${path}/db/${name}.csv`, `\n${data}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   Read(name) {
-    if (!fs.existsSync(`${path}/aero/${name}.csv`)) {
-      throw new Error("Table doesn't exists");
+    try {
+      if (!fs.existsSync(`${path}/db/${name}.csv`)) {
+        throw new Error("Table doesn't exists");
+      }
+
+      let table = fs.readFileSync(`${path}/db/${name}.csv`, {
+        encoding: "utf-8",
+      });
+
+      table = table.split("\n");
+
+      const headers = table[0].split(",");
+      const data = table.filter((_, i) => i !== 0).map((row) => row.split(","));
+
+      return { headers, data };
+    } catch (error) {
+      console.log(error);
     }
-
-    let table = fs.readFileSync(`${path}/aero/${name}.csv`, {
-      encoding: "utf-8",
-    });
-
-    table = table.split("\n");
-
-    const headers = table[0].split(",");
-    const data = table.filter((_, i) => i !== 0).map((row) => row.split(","));
-
-    return { headers, data };
   }
 }
 
